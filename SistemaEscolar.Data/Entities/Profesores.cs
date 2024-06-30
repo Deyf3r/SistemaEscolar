e9db19cf049cf55cac11c4ea3111d30db6ec8491
Iliana2020 <20220056@itla.edu.do>
2024-06-29 21:13:55 -0400

    Reparcion de Estructura CRUD de los Cursos

---
+++ SistemaEscolar.Data/Entities/Profesores.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaEscolar.Data.Entities
+{
+    public class Profesores
+    {
+        [Key]
+        public int ProfesorId { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
+        public string Departamento { get; set; }
+        public int Telefono { get; set; }
+        public string Direccion { get; set; }
+
+
+
+    }
+}
---
+++ SistemaEscolar.Data/Repositories/DB/ProfesoresRepository.cs
@@ -0,0 +1,38 @@
+﻿using SistemaEscolar.Data.Entities;
+using SistemaEscolar.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaEscolar.Data.Repositories.Db
+{
+    public class ProfesoresRepository : IProfesoresRepository
+    {
+        public void Actualizar(Profesores profesores)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Agregar(Profesores profesores)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Profesores ObtenerPorId(int profesorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remover(Profesores profesores)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Profesores> TraerTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
