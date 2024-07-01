2e2ede99c572b377fef0e359ffadf1d65e3516fc
Franklyn Mu?Oz Salcedo <20212296@itla.edu.do>
2024-07-01 11:52:41 -0400

    Agregar Materias

---
+++ SistemaEscolar.Data/Context/AgregarMateriasContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaEscolar.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaEscolar.Data.Context
+{
+    public class AgregarMateriasContext : DbContext
+
+    {
+        public AgregarMateriasContext(DbContextOptions<AgregarMateriasContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("AgregasMaterias");
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<Materias> Materias { get; set; }
+    }
+}
---
+++ SistemaEscolar.Data/Exceptions/MateriaNotExits.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaEscolar.Data.Exceptions
+{
+    public class MateriaNotExitsExeption : Exception
+    {
+        public MateriaNotExitsExeption(string massage) { }
+    }
+}
