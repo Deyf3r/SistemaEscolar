47abffc5210567c22b9b0735892e1f336a3562ba
jimmyguzmant04 <jimmyguzmant04@gmail.com>
2024-07-01 23:11:05 -0400

    Creacion de la tabla usuarios

---
+++ SistemaEscolar.Data/Context/UsuariosContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaEscolar.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaEscolar.Data.Context
+{
+    public class UsuariosContext : DbContext
+    {
+        public UsuariosContext(DbContextOptions<UsuariosContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("EscuelaDB");
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<Usuarios> Usuarios { get; set; }
+
+    }
+}
